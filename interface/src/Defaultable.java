53ede9015ab376cd4f320c0f742fdbf90a268b1b
Giuseppe <giuseppe.riccio@aicof.it>
2024-05-15 11:23:05 +0200

    Fundmental: interfaces

--- interface/src/Defaultable.java
+++ interface/src/Defaultable.java
@@ -6,16 +6,13 @@ Topic:  Default methods
 */
 
 public interface Defaultable {
-
     String abstractValue = "ABSTRACT";
-
     void abstractMethod();
-
     default void defaultNotAbstractMethod() {
         System.out.println("Testing Default");
     }
-
 }
+
 // Class implements Defaultable interface
 class ImplementingClass implements Defaultable {
     public void abstractMethod() {
@@ -24,20 +21,24 @@ class ImplementingClass implements Defaultable {
     }
 }
 
+// Create a base class that has method with same signature default
+// method in interface
+class BaseClass {
+    public void defaultNotAbstractMethod() {
+        System.out.println("Testing defaultNotAbstractMethod on class");
+    }
+}
+
 // Class which uses the interface in a method as a parameter type.
-class DefaultClass {
+class DefaultClass extends BaseClass implements Defaultable {
+
 
     public static void main(String[] args) {
         // Instantiate the current class.
         DefaultClass dc = new DefaultClass();
 
-        // Pass instance of implementing class to method using interface
-        dc.callAbstractMethod(new ImplementingClass());
-
-        // Pass lambda expression to method using interface
-        dc.callAbstractMethod(() -> System.out.println("Lambda Expression" +
-                " implements interface's abstract method"));
-
+        // Call method directly..
+        dc.abstractMethod();
     }
 
     // Method has a parameter of type Defaultable
@@ -45,6 +46,13 @@ class DefaultClass {
         // Execute implementing object's abstractMethod
         d.abstractMethod();
     }
+    public void abstractMethod() {
+        // Which method is this from (interface or BaseClass?)
+        defaultNotAbstractMethod();
+    }
 
 }
 
+/**
+ Testing defaultNotAbstractMethod on class
+ */
